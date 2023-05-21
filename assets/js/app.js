3cb94dd3dd438b77b40f5bbdd974ba8679a05119
DamianoCasolari <damianocasolari@gmail.com>
2023-05-21 17:08:12 +0200

    add nexttick in sameHeight

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,7 @@ createApp({
     },
     methods: {
         sameHeight() {
-            console.log('Pagina completamente caricata.');
             const cards = document.getElementsByClassName('my_card');
-            console.log(cards);
             let maxHeight = 0;
 
             for (let i = 0; i < cards.length; i++) {
@@ -26,33 +24,12 @@ createApp({
         }
     },
     mounted() {
-
-
-        //     setTimeout(() => {
-        //         this.pageLoad = true;
-        //         const cards = document.getElementsByClassName('my_card');
-        //         let maxHeight = 0;
-
-        //         for (let i = 0; i < cards.length; i++) {
-        //             const cardHeight = cards[i].offsetHeight;
-        //             if (cardHeight > maxHeight) {
-        //                 maxHeight = cardHeight;
-        //             }
-        //         }
-
-        //         for (let i = 0; i < cards.length; i++) {
-        //             cards[i].style.minHeight = maxHeight + 'px';
-        //         }
-        //     }, 2000); // Simula un ritardo di caricamento di 2 secondi
-        // }
-
         window.addEventListener('load', () => {
             this.pageLoad = true;
-            this.sameHeight()
-
-
+            this.$nextTick(() => {
+                this.sameHeight();
+            });
         });
-
     }
 }).mount('#app')
 
